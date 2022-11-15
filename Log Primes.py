a09b5368cef8c552758e079d895649a3f8b79311
freddiemoore <101716963+freddiemoore@users.noreply.github.com>
2022-11-15 12:09:37 +0000

    Update Log Primes.py

---
+++ Log Primes.py
@@ -0,0 +1,32 @@
+print("Type input here:")
+n = int(input())
+def log_primes(n):
+    list_primes = []
+    sum_log_primes = 0
+    import math
+    i = 2
+    def test_prime(i):
+        divisor = 2
+        while divisor <= (i/2):
+            if i % divisor == 0:
+                return False
+            else:
+                divisor += 1
+        return True
+    m = len(list_primes)
+    while m <= n:
+        test_prime(i)
+        x = test_prime(i)
+        if x == True:
+            list_primes.append(i)
+            i += 1
+            m = len(list_primes)
+        else:
+            i += 1
+            m = len(list_primes)
+    for c in list_primes:
+        sum_log_primes = math.log(c) + sum_log_primes
+    print(sum_log_primes)
+    print(n)
+    print(sum_log_primes / n)
+log_primes(n)
